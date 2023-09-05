eb29de5e2a510788fed82ab3d6c24ad053d662f6
manju <manju@gmail.com>
2023-09-05 15:58:13 +0530

    Intial commit

---
+++ AS 18/Models/Movies.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace WebAPIEx.Models
+{
+    [Table("Movies")]
+    public class Movies
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTime { get; set; }
+
+    }
+}
+
